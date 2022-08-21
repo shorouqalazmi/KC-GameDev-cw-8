bb3a27bc71649f811b0664d5a62e1ed1a29a7ad8
shorouqalazmi <109908399+shorouqalazmi@users.noreply.github.com>
2022-08-21 16:29:26 +0400

    done

---
+++ cw8/Assets/PLAYER.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYER : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerAnimation();
+    }
+    void PlayerAnimation()
+    {
+        animator.SetFloat("SPEED" , Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("JUMP");
+        }
+    }
+}
